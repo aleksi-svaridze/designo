af2bcbab834380b166972e843e2d2d87208db8d1
aleksi-svaridze <aleksisvaridze@outlook.com>
2023-08-30 14:54:31 +0400

    Finish Portfolio section styling

--- src/layout/portfolio/Portfolio.jsx
+++ src/layout/portfolio/Portfolio.jsx
@@ -1,8 +1,3 @@
-import coverImg1 from '../../images/portfolio/cover_1.png'
-import coverImgSm1 from '../../images/portfolio/cover_1_sm.png'
-
-import coverImg2 from '../../images/portfolio/cover_2.png'
-import coverImg3 from '../../images/portfolio/cover_3.png'
 import arrowIcon from '../../images/portfolio/arrow_icon.svg'
 
 
@@ -14,9 +9,7 @@ const Portfolio = () => {
                 <div className="row gy-4 gy-lg-0">
                     <div className="col-12 col-lg-6">
 
-                        <div className="card-portfolio overflow-hidden position-relative">
-                            <img src={coverImg1} alt="" className='card-portfolio__img d-sm-block d-none' />
-                            <img src={coverImgSm1} alt="" className='card-portfolio__img d-sm-none d-block' />
+                        <div className="card-portfolio card-portfolio--img-1 overflow-hidden position-relative">
                             <div className="overlay"></div>
                             <div className="card-portfolio__text-content position-absolute start-50 top-50 translate-middle text-center w-100">
                                 <h2 className='card-portfolio__text-content--title text-white fw-medium text-uppercase mb-2 pb-1 mb-lg-4'>WEB DESIGN</h2>
@@ -30,8 +23,7 @@ const Portfolio = () => {
                     <div className="col-12 col-lg-6">
                         <div className="row gy-4">
                             <div className="col-12">
-                                <div className="card-portfolio overflow-hidden position-relative">
-                                    <img src={coverImg2} alt="" className='w-100' />
+                                <div className="card-portfolio card-portfolio--img-2 overflow-hidden position-relative">
                                     <div className="overlay"></div>
                                     <div className="card-portfolio__text-content position-absolute start-50 top-50 translate-middle text-center w-100">
                                         <h2 className='card-portfolio__text-content--title text-white fw-medium text-uppercase mb-2 pb-1 mb-lg-4'>APP DESIGN</h2>
@@ -42,8 +34,7 @@ const Portfolio = () => {
                                 </div>
                             </div>
                             <div className="col-12">
-                                <div className="card-portfolio overflow-hidden position-relative">
-                                    <img src={coverImg3} alt="" className='w-100' />
+                                <div className="card-portfolio card-portfolio--img-3 overflow-hidden position-relative">
                                     <div className="overlay"></div>
                                     <div className="card-portfolio__text-content position-absolute start-50 top-50 translate-middle text-center w-100">
                                         <h2 className='card-portfolio__text-content--title text-white fw-medium text-uppercase mb-2 pb-1 mb-lg-4'>GRAPHIC DESIGN</h2>
