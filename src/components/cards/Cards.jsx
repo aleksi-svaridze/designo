914c5b27921d6553b23b0143f9d1883481f25382
aleksi-svaridze <aleksisvaridze@outlook.com>
2023-09-01 13:30:03 +0400

    Add images to worksCard components

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -126,16 +126,16 @@ export const AboutCard = ({id, img, title, desc}) => {
     )
 }
 
-export const WorksCard = ({img}) => {
+export const WorksCard = ({img, id, desc, title}) => {
     return(
-        <div className="col-12 col-lg-6 col-xl-4">
+        <div className="col-12 col-lg-6 col-xl-4" key={id}>
             <div className="works-card overflow-hidden d-flex flex-column flex-sm-row flex-lg-column">
                 <div className="works-card--img-content">
                     <img src={img} alt="" />
                 </div>
                 <div className="works-card--text-content bg-tertiary d-flex flex-column align-items-center justify-content-center text-center">
-                    <h2 className='works-card--title fw-medium text-primary text-uppercase'>express</h2>
-                    <p className='works-card--desc text-gray-dark m-0'>A multi-carrier shipping website for ecommerce businesses</p>
+                    <h2 className='works-card--title fw-medium text-primary text-uppercase'>{title}</h2>
+                    <p className='works-card--desc text-gray-dark m-0'>{desc}</p>
                 </div>
             </div>
         </div>
--- src/layout/worksCardContainer/WorksCardContainer.jsx
+++ src/layout/worksCardContainer/WorksCardContainer.jsx
@@ -1,80 +1,35 @@
-// import { WorksCard } from "../../components/cards/Cards";
+import { WorksCard } from "../../components/cards/Cards";
 
-import img1 from '../../images/works/Express.jpg'
+import img1 from '../../images/works/Express1.jpg'
+import img2 from '../../images/works/Express2.jpg'
+import img3 from '../../images/works/Express3.jpg'
+import img4 from '../../images/works/Express4.jpg'
+import img5 from '../../images/works/Express5.jpg'
+import img6 from '../../images/works/Express6.jpg'
 
 
 const WorksCardContainer = () => {
+    let webDesignData = [
+        {id: 1, img: img1, title: 'express', desc: 'A multi-carrier shipping website for ecommerce businesses'},
+        {id: 2, img: img2, title: 'TRANSFER', desc: 'Site for low-cost money transfers and sending money within seconds'},
+        {id: 3, img: img3, title: 'PHOTON', desc: 'A state-of-the-art music player with high-resolution audio and DSP effects'},
+        {id: 4, img: img4, title: 'BUILDER', desc: 'Connects users with local contractors based on their location'},
+        {id: 5, img: img5, title: 'BLOGR', desc: 'Blogr is a platform for creating an online blog or publication'},
+        {id: 6, img: img6, title: 'CAMP', desc: 'Get expert training in coding, data, design, and digital marketing'},
+    ]
     return(
         <div className="container works-card-container">
             <div className="row">
-                {/* <WorksCard img={img1} /> */}
-                <div className="col-12 col-lg-6 col-xl-4">
-            <div className="works-card overflow-hidden d-flex flex-column flex-sm-row flex-lg-column">
-                <div className="works-card--img-content">
-                    <img src={img1} alt="" />
-                </div>
-                <div className="works-card--text-content bg-tertiary d-flex flex-column align-items-center justify-content-center text-center">
-                    <h2 className='works-card--title fw-medium text-primary text-uppercase'>express</h2>
-                    <p className='works-card--desc text-gray-dark m-0'>A multi-carrier shipping website for ecommerce businesses</p>
-                </div>
-            </div>
-        </div>
-        <div className="col-12 col-lg-6 col-xl-4">
-            <div className="works-card overflow-hidden d-flex flex-column flex-sm-row flex-lg-column">
-                <div className="works-card--img-content">
-                    <img src={img1} alt="" />
-                </div>
-                <div className="works-card--text-content bg-tertiary d-flex flex-column align-items-center justify-content-center text-center">
-                    <h2 className='works-card--title fw-medium text-primary text-uppercase'>express</h2>
-                    <p className='works-card--desc text-gray-dark m-0'>A multi-carrier shipping website for ecommerce businesses</p>
-                </div>
-            </div>
-        </div>
-        <div className="col-12 col-lg-6 col-xl-4">
-            <div className="works-card overflow-hidden d-flex flex-column flex-sm-row flex-lg-column">
-                <div className="works-card--img-content">
-                    <img src={img1} alt="" />
-                </div>
-                <div className="works-card--text-content bg-tertiary d-flex flex-column align-items-center justify-content-center text-center">
-                    <h2 className='works-card--title fw-medium text-primary text-uppercase'>express</h2>
-                    <p className='works-card--desc text-gray-dark m-0'>A multi-carrier shipping website for ecommerce businesses</p>
-                </div>
-            </div>
-        </div>
-        <div className="col-12 col-lg-6 col-xl-4">
-            <div className="works-card overflow-hidden d-flex flex-column flex-sm-row flex-lg-column">
-                <div className="works-card--img-content">
-                    <img src={img1} alt="" />
-                </div>
-                <div className="works-card--text-content bg-tertiary d-flex flex-column align-items-center justify-content-center text-center">
-                    <h2 className='works-card--title fw-medium text-primary text-uppercase'>express</h2>
-                    <p className='works-card--desc text-gray-dark m-0'>A multi-carrier shipping website for ecommerce businesses</p>
-                </div>
-            </div>
-        </div>
-        <div className="col-12 col-lg-6 col-xl-4">
-            <div className="works-card overflow-hidden d-flex flex-column flex-sm-row flex-lg-column">
-                <div className="works-card--img-content">
-                    <img src={img1} alt="" />
-                </div>
-                <div className="works-card--text-content bg-tertiary d-flex flex-column align-items-center justify-content-center text-center">
-                    <h2 className='works-card--title fw-medium text-primary text-uppercase'>express</h2>
-                    <p className='works-card--desc text-gray-dark m-0'>A multi-carrier shipping website for ecommerce businesses</p>
-                </div>
-            </div>
-        </div>
-        <div className="col-12 col-lg-6 col-xl-4">
-            <div className="works-card overflow-hidden d-flex flex-column flex-sm-row flex-lg-column">
-                <div className="works-card--img-content">
-                    <img src={img1} alt="" />
-                </div>
-                <div className="works-card--text-content bg-tertiary d-flex flex-column align-items-center justify-content-center text-center">
-                    <h2 className='works-card--title fw-medium text-primary text-uppercase'>express</h2>
-                    <p className='works-card--desc text-gray-dark m-0'>A multi-carrier shipping website for ecommerce businesses</p>
-                </div>
-            </div>
-        </div>
-        
+                {
+                    webDesignData.map(item => (
+                        <WorksCard 
+                            img={item.img} 
+                            title={item.title} 
+                            id={item.id} 
+                            desc={item.desc}    
+                        />       
+                    ))
+                }
             </div>
         </div>
     )
