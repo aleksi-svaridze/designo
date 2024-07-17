21088a4f5e07f4183e76631e82cbd157f84ab09f
aleksi-svaridze <aleksisvaridze@outlook.com>
2024-07-17 20:56:31 +0400

    Add pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-
+import { Routes, Route } from "react-router-dom";
+import { About } from "./pages/About";
+import { Location } from "./pages/Location";
+import { Contact } from "./pages/Contact";
+import { NotFound } from "./pages/NotFound";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
-    <div className="text-red-500">
-      hello world
+    <div>
+      <Header />
+      <Routes>
+        <Route path="" index element={<About />} />
+        <Route path="locations"  element={<Location />} />        
+        <Route path="contact"  element={<Contact />} />
+        <Route path="*"  element={<NotFound />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
