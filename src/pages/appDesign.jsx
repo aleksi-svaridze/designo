f0b7e022d66c7f87533892e654c411ad5d2bf869
aleski-svaridze <aleksisvaridze@outlook.com>
2024-10-07 20:58:15 +0400

    Add new data to app-design page

--- src/pages/appDesign.jsx
+++ src/pages/appDesign.jsx
@@ -2,43 +2,49 @@ import { useState } from "react";
 import { DesignCards } from "../components/Cards";
 import { SecondaryHero } from "../components/Heroes";
 
+import AirFilterImage from '../images/cards/app-design-cards/airfilter.jpg'
+import EyeCamImage from '../images/cards/app-design-cards/eyecam.jpg'
+import TodoImage from '../images/cards/app-design-cards/todo.jpg'
+import FaceItmage from '../images/cards/app-design-cards/faceit.jpg'
+import LoopStudiosImage from '../images/cards/app-design-cards/loopstudio.jpg'
+
 export const AppDesign = () => {
-  // const [webDesignCardsContent] = useState([
-  //   {
-  //     id: 1,
-  //     imageSrc: imageExpress,
-  //     title: "express",
-  //     description: "A multi-carrier shipping website for ecommerce businesses",
-  //   },
-  //   {
-  //     id: 2,
-  //     imageSrc: imageTransfer,
-  //     title: "transfer",
-  //     description:
-  //       "Site for low-cost money transfers and sending money within seconds",
-  //   },
-  //   {
-  //     id: 3,
-  //     imageSrc: imagePhoton,
-  //     title: "photon",
-  //     description:
-  //       "A state-of-the-art music player with high-resolution audio and DSP effects",
-  //   },
-  //   {
-  //     id: 4,
-  //     imageSrc: imageBuilder,
-  //     title: "builder",
-  //     description:
-  //       "Connects users with local contractors based on their location",
-  //   },
-  //   {
-  //     id: 5,
-  //     imageSrc: imageBlogr,
-  //     title: "blogr",
-  //     description:
-  //       "Blogr is a platform for creating an online blog or publication",
-  //   },
-  // ]);
+  const [appDesignCardsContent] = useState([
+    {
+      id: 1,
+      imageSrc: AirFilterImage,
+      title: "AIRFILTER",
+      description: "Solving the problem of poor indoor air quality by filtering the air",
+    },
+    {
+      id: 2,
+      imageSrc: EyeCamImage,
+      title: "EYECAM",
+      description:
+        "Product that lets you edit your favorite photos and videos at any time",
+    },
+    {
+      id: 3,
+      imageSrc: FaceItmage,
+      title: "FACEIT",
+      description:
+        "Get to meet your favorite internet superstar with the faceit app",
+    },
+    {
+      id: 4,
+      imageSrc: TodoImage,
+      title: "TODO",
+      description:
+        "A todo app that features cloud sync with light and dark mode",
+    },
+    {
+      id: 5,
+      imageSrc: LoopStudiosImage,
+      title: "LOOPSTUDIOS",
+      description:
+        "A VR experience app made for Loopstudios",
+    },
+  ]);
   return (
     <div className="md:container md:px-10 xl:px-[85px]">
       <SecondaryHero
@@ -48,9 +54,9 @@ export const AppDesign = () => {
         }
       />
       <div className="container py-24 md:py-[120px] lg:py-[160px] px-6 md:px-0 grid lg:grid-cols-3 gap-y-10 lg:gap-y-8 lg:gap-x-[30px]">
-        {/* {webDesignCardsContent.map((item) => ( */}
-          {/* <DesignCards item={item} /> */}
-        {/* ))} */}
+        {appDesignCardsContent.map((item) => (
+          <DesignCards item={item} />
+        ))}
       </div>
     </div>
   );
